80c8eb8bae193005cc1c57d9f9b0b8fe58f2e7cf
Dimon Eir <deiramjani@gmail.com>
2021-06-26 03:41:41 -0700

    Mongoose 2

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
-        name: 'React Lake Campground',
+    Campsite.create({
+        name: 'React Lake Campgroundd',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
